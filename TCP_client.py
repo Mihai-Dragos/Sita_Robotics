c5fd7cc6b31fbd1bbcb7098027f3c3185182d7dc
marnikdenouden <marnikdenouden@gmail.com>
2024-05-19 11:43:01 +0200

    Update TCP_client.py
    Update private field naming, by making it one underscore.

--- TCP_client.py
+++ TCP_client.py
@@ -59,13 +59,14 @@ class Connection():
         log("Connection", f"Initializing connection for " +
             f"connected socket {connected_socket.getsockname()}")
         self.socket = connected_socket
-        self.__writing__ = True
-        self.__reading__ = True
-        self.__started__ = False
+        self._writing = True
+        self._reading = True
+        self._started = False
+        self._closed = False
 
         #tcp_socket.settimeout(60)
 
-        self.__data_to_send__ = Queue[bytes]()
+        self._data_to_send = Queue[bytes]()
 
         self.receiver = Thread(target=self.__receiver__)
         self.sender = Thread(target=self.__sender__)
@@ -81,29 +82,33 @@ class Connection():
 
     def start(self):
         '''Start the connection to begin reading and writing data'''
-        if not self.__started__:
+        if not self._started:
             debug_log("Connection", f"Starting receiver and sender thread")
             self.receiver.start()
             self.sender.start()
-            self.__started__ = True
+            self._started = True
 
     def __stop_reading__(self):
         '''Stop the connection from reading on the socket'''
-        if (self.__reading__):
+        if (self._reading):
             debug_log("Connection", "Stop reading from the socket")
-            self.__reading__ = False
+            self._reading = False
             self.socket.shutdown(SHUT_RD)
     
     def __stop_writing__(self):
         '''Stop the connection from writing on the socket'''
-        if (self.__writing__):
+        if (self._writing):
             debug_log("Connection", "Stop writing to the socket")
-            self.__writing__ = False
+            self._writing = False
             self.send_message("") # Unblock sender from waiting
             self.socket.shutdown(SHUT_WR)
         
     def close(self):
         '''Close the connection of the socket to the current server'''
+        if (self._closed):
+            debug_log("Connection", "Connection has already been closed")
+            return
+        
         log("Connection", f"Closing the TCP connection with socket {self.socket.getsockname()}")
         
         self.__stop_writing__()
@@ -113,41 +118,42 @@ class Connection():
         debug_log("Connection", f"Waiting for reading to stop " +
                                  f"from EOF signal or 4 second timeout")
         self.socket.settimeout(4)
-        while(self.__reading__):
+        while(self._reading):
             time.sleep(0.1)
         
         debug_log("Connection", "Waiting for receiver thread to join")
         self.receiver.join()
             
-        debug_log("Connection", f"Closing connection to socket {self.socket.getsockname()}")
+        debug_log("Connection", f"Closing connection at {self.socket.getsockname()} socket to address {self.socket.getpeername()}")
         self.socket.close()
         Message.remove_end_of_file_listener(self.end_listener)
+        self._closed = True
 
     def send_message(self, data:bytes):
         '''Add a message to the connection to send'''
         debug_log("Connection", f"Adding message to the queue:")
         debug_log("", f"\"{data}\"")
-        self.__data_to_send__.put(data)
+        self._data_to_send.put(data)
 
-    __receive_listeners__ =  list[Callable[[bytes], None]]()
+    _receive_listeners =  list[Callable[[bytes], None]]()
     '''List of listeners for receiving connection data'''
 
     def add_receive_listener(self, listener: Callable[[bytes], None]):
         '''Add listener for receiving connection data'''
-        self.__receive_listeners__.append(listener)
+        self._receive_listeners.append(listener)
 
     def __receiver__(self):
         '''Program to run on a thread, which continously receives messages from the connection'''
         debug_log("Receiver", f"Start of receiver thread")
 
-        while self.__reading__:
+        while self._reading:
             received_data = self.__receive__()
             debug_log("Receiver", f"Received message from the connection")
         
             if (not received_data): 
                 continue
         
-            for listener in self.__receive_listeners__:
+            for listener in self._receive_listeners:
                 listener(received_data)
 
         debug_log("Receiver", f"Reached end of receiver thread")
@@ -157,10 +163,10 @@ class Connection():
         debug_log("Sender", f"Start of sender thread")
 
         while True:
-            message = self.__data_to_send__.get()
+            message = self._data_to_send.get()
             debug_log("Sender", f"Got new data to send from queue")
 
-            if (not self.__writing__): 
+            if (not self._writing): 
                 break # End sender thread as we should stop writing 
             
             log("Sender", "Sending message:")
