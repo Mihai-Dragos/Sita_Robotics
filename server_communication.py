52b043375efe01a0f31ceb2b4a265680064d461a
marnikdenouden <marnikdenouden@gmail.com>
2024-05-19 11:44:32 +0200

    Update server_communication.py
    Add channel communication, basically a class that utilizes the connection class to transmit strings.

--- server_communication.py
+++ server_communication.py
@@ -1,5 +1,38 @@
 from TCP_client import *
 
-connection = create_tcp_connection()
+class Channel():
+    def __init__(self):
+        self.connection = create_tcp_connection()
+        self.connection.add_receive_listener(Channel.on_receive_message)
 
-connection.send_message(str.encode("message", "utf-8"))
+    def send_message(self, message:str):
+        log("Channel", f"Sending message:")
+        log("", f"{message}")
+        self.connection.send_message(message.encode("utf-8"))
+
+    def on_receive_message(data:bytes):
+        log("Channel", f"Received message")
+        log("", f"{data.decode("utf-8")}")
+
+    def close(self):
+        self.connection.close()
+
+    def is_finished(self):
+        return not self.connection._reading and not self.connection._writing
+
+channel = Channel()
+while True:
+    time.sleep(0.1)
+    log("TCP Client", f"Waiting for message or type 'stop' to end connection:")
+    input_string = input()
+    if (input_string == "stop"): break
+    if (input_string == "debug"): 
+        DEBUG_TCP_CLIENT = not DEBUG_TCP_CLIENT
+        continue
+    if (channel.is_finished()):
+        channel.close()
+        break
+    channel.send_message(input_string)
+
+debug_log("TCP Client", f"Closing connection")
+channel.close()
