d040efe891ccbd23facda3161fd1c8a8acb4826a
marnikdenouden <marnikdenouden@gmail.com>
2024-06-01 16:40:42 +0200

    Update data_export.py
    Update export file address path.

--- data_export.py
+++ data_export.py
@@ -1,14 +1,19 @@
 from data_values import export_data
 from settings import EXPORT_FILE_ADDRESS
+import os
 
-def exportString(fileName:str, string:str):
+def getExportFileAddress():
+   file_address = f"{os.path.dirname(__file__)}/{EXPORT_FILE_ADDRESS}.csv"
+   return file_address
+
+def exportString(fileAddress:str, string:str):
     '''Append a specified string to the specified file.'''
-    with open(fileName + ".csv", "a") as outfile:
+    with open(fileAddress, "a") as outfile:
         outfile.write(string)
 
 def exportValue(value:str):
     '''Export a specified string value to the export file.'''
-    exportString(EXPORT_FILE_ADDRESS, value + "\n")
+    exportString(getExportFileAddress(), value + "\n")
 
 def exportHeader():
     '''Write the header string for the current export data to the export file.'''
@@ -20,5 +25,5 @@ def exportData():
 
 def clearData():
     '''Clear the export file from all previous data.'''
-    with open(EXPORT_FILE_ADDRESS + ".csv", "w") as outfile:
+    with open(getExportFileAddress(), "w") as outfile:
         outfile.write("")
